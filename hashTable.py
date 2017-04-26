ff35d6dd6850201d1ae15161680801db5b237f9a
大数据挖掘sparkexpert <277433109@qq.com>
2017-04-26 09:44:39 +0800

    进行了RAISR的测试
    本测试代码来源于：Lhttps://github.com/MKFMIKU/RAISR
    
    并对其中的一些地方进行了修改，主要是由于本机运行于python3.6环境下，因此一些PYTHON脚本问题不太一致

---
+++ hashTable.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def hashTable(patch,Qangle,Qstrenth,Qcoherence):
+    [gx,gy] = np.gradient(patch)
+    G = np.matrix((gx.ravel(),gy.ravel())).T
+    x = G.T*G
+    [eigenvalues,eigenvectors] = np.linalg.eig(x)
+    
+    #For angle
+    angle = np.math.atan2(eigenvectors[0,1],eigenvectors[0,0])
+    if angle<0:
+        angle += np.pi
+    
+    #For strength
+    strength = eigenvalues.max()/(eigenvalues.sum()+0.0001)
+    
+    #For coherence
+    lamda1 = np.math.sqrt(eigenvalues.max())
+    lamda2 = np.math.sqrt(eigenvalues.min())
+    coherence = np.abs((lamda1-lamda2)/(lamda1+lamda2+0.0001))
+    
+    #Quantization
+    angle = np.floor(angle/(np.pi/Qangle)-1)
+    strength = np.floor(strength/(1/Qstrenth)-1)
+    coherence = np.floor(coherence/(1/Qcoherence)-1)
+    
+    return int(angle),int(strength),int(coherence)
---
+++ train.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import cv2
+#import scipy as scipy
+import scipy.sparse as sparse
+from scipy.sparse.linalg import cg
+#from scipy import sparse
+
+from hashTable import hashTable
+
+
+Qangle = 24
+Qstrenth = 3
+Qcoherence = 3
+
+#构建空的矩阵Q，V用了存对应的LR和HR，h为过滤器 三个hashmap分别为Angle，Strength，Coherence，t
+Q = np.zeros((Qangle*Qstrenth*Qcoherence,4,11*11,11*11))
+V = np.zeros((Qangle*Qstrenth*Qcoherence,4,11*11,1))
+h = np.zeros((Qangle*Qstrenth*Qcoherence,4,11*11))
+
+
+dataDir="./train"
+fileList = []
+for parent,dirnames,filenames in os.walk(dataDir):
+    for filename in filenames:
+        fileList.append(os.path.join(parent,filename))
+
+for file in fileList:
+    print("HashMap of %s"%file)
+    mat = cv2.imread(file)
+    #转换色彩空间，只对亮度进行训练
+    mat = cv2.cvtColor(mat, cv2.COLOR_BGR2YCrCb)[:,:,2]
+    #放缩为0-1
+    mat = cv2.normalize(mat.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    HR = mat
+
+    #模糊LR
+    #Upscaling
+    #cv2.GaussianBlur()
+    LR = cv2.GaussianBlur(HR,(0,0),2);
+    #LR = cv2.GaussianBlur(LR,(0,0),2);
+
+    # Set the train map
+
+    #遍历每个像素
+    for xP in range(5,LR.shape[0]-6):
+        for yP in range(5,LR.shape[1]-6):
+            #取patch
+            patch = LR[xP-5:xP+6,yP-5:yP+6]
+            #计算特征
+            [angle,strenth,coherence] = hashTable(patch,Qangle,Qstrenth,Qcoherence)
+            #压缩向量空间
+            j = angle*9+strenth*3+coherence
+            A = patch.reshape(1,-1)
+            x = HR[xP][yP]
+            #计算像素类型
+            t = xP%2*2+yP%2
+            #存入对应的HashMap
+            Q[j,t] += A*A.T
+            V[j,t] += A.T*x
+
+
+# Set the train step
+for t in range(4):
+    for j in range(Qangle*Qstrenth*Qcoherence):
+        #针对每种像素类型和图像特征训练4*24*3*3个过滤器
+        h[j,t] = cg(Q[j,t],V[j,t])[0]
+        #h[j,t] = sparse.linalg.cg(Q[j,t],V[j,t])[0]
+
+print("Train is off")
+np.save("./lowR2",h)
